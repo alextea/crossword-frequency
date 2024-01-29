98503e38f373b941948f0333b26e3c03850d8b81
Alex Torrance <hello@alextorrance.co.uk>
2024-01-29 21:17:43 +0000

    Construct full Postgresql URL from environment variables

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+import urllib.parse
 
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -10,7 +11,12 @@ class DevelopmentConfig(Config):
 
 class ProductionConfig(Config):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    database_name = os.environ.get('DATABASE_NAME')
+    database_user = os.environ.get('DATABASE_USER')
+    database_pass = urllib.parse.quote(os.environ.get('DATABASE_PASSWORD'))
+    database_host = 'localhost'
+    
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{database_user}:{database_pass}@{database_host}/{database_name}"
     PORT = os.environ.get('PORT', 5000)
 
 # Determine the environment based on an environment variable
